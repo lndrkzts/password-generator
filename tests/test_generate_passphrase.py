5a61deb56aa0f0c2e4ffe9bc88566b79f53e0e2a
lndrkzts <64717717+lndrkzts@users.noreply.github.com>
2023-05-28 23:02:13 -0300

    added passphrase_generator tests

---
+++ tests/test_generate_passphrase.py
@@ -0,0 +1,33 @@
+import unittest
+
+from src import counters
+from src.passphrase import passphrase_generator
+
+
+class TestGeneratePassphrase(unittest.TestCase):
+
+    def test_default_passphrase_generation(self):
+        passphrase = passphrase_generator()
+        words_count = counters.words_count(passphrase)
+        self.assertEqual(words_count, 5)
+
+    def test_words(self):
+        passphrase = passphrase_generator(words=8)
+        words_count = counters.words_count(passphrase)
+        self.assertEqual(words_count, 8)
+
+    def test_separator(self):
+        separator = '@'
+        passphrase = passphrase_generator(words=5, separator=separator)
+        separator_count = counters.char_count(passphrase, char=separator)
+        self.assertEqual(separator_count, 4)
+
+    def test_capitalize(self):
+        passphrase = passphrase_generator(words=4, capitalize=True)
+        upper_count = counters.upper_count(passphrase)
+        self.assertEqual(upper_count, 4)
+
+    def test_include_number(self):
+        passphrase = passphrase_generator(include_number=True)
+        numbers_count = counters.numbers_count(passphrase)
+        self.assertEqual(numbers_count, 1)
